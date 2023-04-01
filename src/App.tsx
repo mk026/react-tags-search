61a3f7854dfcfd62de0a0eaeef853c5eff5d0dff
Mikhail <mk0602604@gmail.com>
2023-04-01 23:15:54 +0300

    Create articles/search-articles-form components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Articles from "./components/articles";
+import Layout from "./components/layout";
+import SearchArticlesForm from "./components/search-articles-form";
+
 function App() {
-  return <div>React Tags Search</div>;
+  return (
+    <Layout>
+      <SearchArticlesForm />
+      <Articles />
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/components/search-articles-form/SearchArticlesForm.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const SearchArticlesForm: FC = () => {
+  return <div>SearchArticlesForm</div>;
+};
+
+export default SearchArticlesForm;
