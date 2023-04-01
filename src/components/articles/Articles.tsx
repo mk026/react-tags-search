2aa40d64e8bd0b86dd1e1496c2b9e59f95967a96
Mikhail <mk0602604@gmail.com>
2023-04-01 23:28:01 +0300

    Create article component

--- src/components/articles/Articles.tsx
+++ src/components/articles/Articles.tsx
@@ -1,7 +1,20 @@
 import { FC } from "react";
 
-const Articles: FC = () => {
-  return <div>Articles</div>;
+import { IArticle } from "../../types/article";
+import Article from "./article";
+
+interface ArticlesProps {
+  articles: IArticle[];
+}
+
+const Articles: FC<ArticlesProps> = ({ articles }) => {
+  return (
+    <ul>
+      {articles.map((article) => (
+        <Article key={article.id} article={article} />
+      ))}
+    </ul>
+  );
 };
 
 export default Articles;
---
+++ src/components/articles/article/Article.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+
+import { IArticle } from "../../../types/article";
+
+interface ArticleProps {
+  article: IArticle;
+}
+
+const Article: FC<ArticleProps> = ({ article }) => {
+  return (
+    <>
+      <p>{article.title}</p>
+      <p>{article.content}</p>
+    </>
+  );
+};
+
+export default Article;
