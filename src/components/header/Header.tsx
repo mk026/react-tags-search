931e9b918872a825bd76d90a2b4346a65b0d007a
Mikhail <mk0602604@gmail.com>
2023-04-16 01:27:11 +0300

    Create app-title component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
 
+import AppTitle from "./app-title";
+
 const Header: FC = () => {
   return (
     <div>
-      <h1>React Tags Search</h1>
+      <AppTitle />
     </div>
   );
 };
---
+++ src/components/header/app-title/AppTitle.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const AppTitle: FC = () => {
+  return <h1>React Tags Search</h1>;
+};
+
+export default AppTitle;
