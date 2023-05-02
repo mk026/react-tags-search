18c40c3a56a6cd462ceb75e8450797817debff0a
Mikhail <mk0602604@gmail.com>
2023-05-02 23:03:06 +0300

    Add default custom button styles

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,9 +1,18 @@
 import { ButtonHTMLAttributes, FC } from "react";
 
+import classes from "./Button.module.css";
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button: FC<ButtonProps> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
+const Button: FC<ButtonProps> = ({ children, className, ...props }) => {
+  return (
+    <button
+      className={className ? classes.button + " " + className : classes.button}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
