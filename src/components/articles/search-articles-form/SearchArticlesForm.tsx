6062c0c35b266bfd7103bead21d26461be7e1c0d
Mikhail <mk0602604@gmail.com>
2023-05-02 22:40:17 +0300

    Add styles for search-articles-form

--- src/components/articles/search-articles-form/SearchArticlesForm.tsx
+++ src/components/articles/search-articles-form/SearchArticlesForm.tsx
@@ -1,7 +1,20 @@
 import { FC } from "react";
 
+import classes from "./SearchArticleForm.module.css";
+
 const SearchArticlesForm: FC = () => {
-  return <div>SearchArticlesForm</div>;
+  return (
+    <div className={classes["search-articles"]}>
+      <form>
+        <input
+          type="text"
+          placeholder="Search articles..."
+          className={classes["search-articles__input"]}
+        />
+        <button className={classes["search-articles__button"]}>Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchArticlesForm;
