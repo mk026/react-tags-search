f82b6763900472e6e2dd97a0f1073bb92b1526b4
Mikhail <mk0602604@gmail.com>
2023-05-26 23:14:13 +0300

    Refactor article component

--- src/components/articles/articles-grid/article/Article.tsx
+++ src/components/articles/articles-grid/article/Article.tsx
@@ -1,19 +1,33 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import { IArticle } from "../../../../types/article";
 import Card from "../../../ui/card";
 
 import classes from "./Article.module.css";
+import Button from "../../../ui/button/Button";
 
 interface ArticleProps {
   article: IArticle;
+  expand?: boolean;
 }
 
 const Article: FC<ArticleProps> = ({ article }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  let { content } = article;
+  const isExpandable = content.length > 200;
+
+  if (isExpandable && !isExpanded) {
+    content = content.slice(0, 200) + "...";
+  }
+
   return (
     <Card className={classes.article}>
       <p className={classes["article__header"]}>{article.title}</p>
-      <p className={classes["article__content"]}>{article.content}</p>
+      <p className={classes["article__content"]}>{content}</p>
+      {isExpandable && !isExpanded && (
+        <Button onClick={() => setIsExpanded(true)}>Read more</Button>
+      )}
     </Card>
   );
 };
