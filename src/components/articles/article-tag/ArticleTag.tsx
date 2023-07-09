a615c353533556bd534a75a531c9da18815a3535
Mikhail <mk0602604@gmail.com>
2023-07-09 21:35:58 +0300

    Add styles for article-tag component

--- src/components/articles/article-tag/ArticleTag.tsx
+++ src/components/articles/article-tag/ArticleTag.tsx
@@ -2,12 +2,14 @@ import { FC } from "react";
 
 import { ITag } from "../../../types/tag";
 
+import classes from "./ArticleTag.module.css";
+
 interface ArticleTagProps {
   tag: ITag;
 }
 
 const ArticleTag: FC<ArticleTagProps> = ({ tag }) => {
-  return <div>{tag.title}</div>;
+  return <div className={classes.tag}>{tag.title}</div>;
 };
 
 export default ArticleTag;
