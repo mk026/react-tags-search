1db0fe308b045d7eba6ab11cf13f18e18a78917f
Mikhail <mk0602604@gmail.com>
2023-07-10 22:26:16 +0300

    Add styles for app-info component

--- src/components/app-info/AppInfo.tsx
+++ src/components/app-info/AppInfo.tsx
@@ -1,7 +1,15 @@
 import { FC } from "react";
 
+import Card from "../ui/card";
+
+import classes from "./AppInfo.module.css";
+
 const AppInfo: FC = () => {
-  return <div>Welcome to React Tags Search App</div>;
+  return (
+    <Card className={classes.info}>
+      <h2 className={classes.title}>Welcome to React Tags Search App</h2>
+    </Card>
+  );
 };
 
 export default AppInfo;
