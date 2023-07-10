25f892e8f6120a8317f2d783f8a717081540618b
Mikhail <mk0602604@gmail.com>
2023-07-10 22:21:09 +0300

    Add styles for search-suggestions component

--- src/components/articles/search-articles-form/search-suggestions/SearchSuggestions.tsx
+++ src/components/articles/search-articles-form/search-suggestions/SearchSuggestions.tsx
@@ -1,11 +1,15 @@
 import { FC } from "react";
 
+import classes from "./SearchSuggestions.module.css";
+
 interface SearchSuggestionsProps {
   isOpen: boolean;
 }
 
 const SearchSuggestions: FC<SearchSuggestionsProps> = ({ isOpen }) => {
-  return isOpen ? <div>SearchSuggestions</div> : null;
+  return isOpen ? (
+    <div className={classes.suggestions}>SearchSuggestions</div>
+  ) : null;
 };
 
 export default SearchSuggestions;
