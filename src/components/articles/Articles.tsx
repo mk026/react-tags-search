0926098d2bf4c7d157cd545023f7975869fe70c3
Mikhail <mk0602604@gmail.com>
2023-07-12 22:56:22 +0300

    Render app-info component

--- src/components/articles/Articles.tsx
+++ src/components/articles/Articles.tsx
@@ -1,13 +1,16 @@
 import { FC } from "react";
 
+import AppInfo from "../app-info";
 import SearchArticlesForm from "./search-articles-form";
 import ArticlesGrid from "./articles-grid/ArticlesGrid";
 import { useGetArticlesQuery } from "../../hooks/useGetArticlesQuery";
 
 const Articles: FC = () => {
   const { data } = useGetArticlesQuery();
+
   return (
     <>
+      <AppInfo />
       <SearchArticlesForm />
       <ArticlesGrid articles={data || []} />
     </>
